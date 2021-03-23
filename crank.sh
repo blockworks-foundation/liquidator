432b4acc21aa29b95c8ec125c4158912921b9a4f
dd <dafydd.durairaj@gmail.com>
2021-03-23 15:02:15 -0400

    fetching wallets in crank programmatically

--- crank.sh
+++ crank.sh
@@ -1,38 +1,42 @@
-source ~/mango-client-ts/devnet.env
 
-DEX_PROGRAM_ID=$(cat ~/mango-client-ts/src/ids.json | jq .devnet.dex_program_id -r)
+source ~/mango/cli/devnet.env
+
+DEX_PROGRAM_ID=$(cat $IDS_PATH | jq .devnet.dex_program_id -r)
 
 MARKET_STR="${1^^}/${2^^}"
 
 if [ $MARKET_STR = "BTC/USDT" ]; then
   MARKET=$(cat ~/mango-client-ts/src/ids.json | jq '.devnet.spot_markets|.["BTC/USDT"]' -r)
-  BASE_WALLET=$BTC_WALLET
-  QUOTE_WALLET=$USDT_WALLET
+  BASE_WALLET=$(spl-token accounts --owner $KEYPAIR $BTC | tail -1 | cut -d' ' -f1)
+  QUOTE_WALLET=$(spl-token accounts --owner $KEYPAIR $USDT | tail -1 | cut -d' ' -f1)
 elif [ $MARKET_STR = "ETH/USDT" ]; then
   MARKET=$(cat ~/mango-client-ts/src/ids.json | jq '.devnet.spot_markets|.["ETH/USDT"]' -r)
-  BASE_WALLET=$ETH_WALLET
-  QUOTE_WALLET=$USDT_WALLET
+  BASE_WALLET=$(spl-token accounts --owner $KEYPAIR $ETH | tail -1 | cut -d' ' -f1)
+  QUOTE_WALLET=$(spl-token accounts --owner $KEYPAIR $USDT | tail -1 | cut -d' ' -f1)
 elif [ $MARKET_STR = "BTC/USDC" ]; then
   MARKET=$(cat ~/mango-client-ts/src/ids.json | jq '.devnet.spot_markets|.["BTC/USDC"]' -r)
-  BASE_WALLET=$BTC_WALLET
-  QUOTE_WALLET=$USDC_WALLET
+  BASE_WALLET=$(spl-token accounts --owner $KEYPAIR $BTC | tail -1 | cut -d' ' -f1)
+  QUOTE_WALLET=$(spl-token accounts --owner $KEYPAIR $USDC | tail -1 | cut -d' ' -f1)
+
 elif [ $MARKET_STR = "ETH/USDC" ]; then
   MARKET=$(cat ~/mango-client-ts/src/ids.json | jq '.devnet.spot_markets|.["ETH/USDC"]' -r)
-  BASE_WALLET=$ETH_WALLET
-  QUOTE_WALLET=$USDC_WALLET
+  BASE_WALLET=$(spl-token accounts --owner $KEYPAIR $ETH | tail -1 | cut -d' ' -f1)
+  QUOTE_WALLET=$(spl-token accounts --owner $KEYPAIR $USDC | tail -1 | cut -d' ' -f1)
+
 elif [ $MARKET_STR = "BTC/WUSDT" ]; then
   MARKET=$(cat ~/mango-client-ts/src/ids.json | jq '.devnet.spot_markets|.["BTC/WUSDT"]' -r)
-  BASE_WALLET=$BTC_WALLET
-  QUOTE_WALLET=$WUSDT_WALLET
+
+  BASE_WALLET=$(spl-token accounts --owner $KEYPAIR $BTC | tail -1 | cut -d' ' -f1)
+  QUOTE_WALLET=$(spl-token accounts --owner $KEYPAIR $WUSDT | tail -1 | cut -d' ' -f1)
 elif [ $MARKET_STR = "ETH/WUSDT" ]; then
   MARKET=$(cat ~/mango-client-ts/src/ids.json | jq '.devnet.spot_markets|.["ETH/WUSDT"]' -r)
-  BASE_WALLET=$ETH_WALLET
-  QUOTE_WALLET=$WUSDT_WALLET
+  BASE_WALLET=$(spl-token accounts --owner $KEYPAIR $ETH | tail -1 | cut -d' ' -f1)
+  QUOTE_WALLET=$(spl-token accounts --owner $KEYPAIR $WUSDT | tail -1 | cut -d' ' -f1)
 else
   echo "invalid args"
 fi
 
 
-cd ~/blockworks-foundation/serum-dex/dex/crank
-
+pushd ~/blockworks-foundation/serum-dex/dex/crank || exit
 cargo run -- $CLUSTER consume-events --dex-program-id $DEX_PROGRAM_ID --payer $KEYPAIR --market $MARKET --coin-wallet $BASE_WALLET --pc-wallet $QUOTE_WALLET --num-workers 1 --events-per-worker 5 --log-directory .
+popd
